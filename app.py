39a1d535bd543dc1075c33cad7863436c3ba455a
Paul R <paulyacha@gmail.com>
2025-03-07 13:21:48 +0100

    no commit

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import pandas as pd
 import requests
 import numpy as np
 from datetime import datetime
-
+import folium
+from streamlit_folium import st_folium
 
 '''
 # SPACE TRAVEL TAXI CO.
@@ -12,7 +13,7 @@ from datetime import datetime
 st.markdown(f'''
 We are a space travel company,
 
-Please provide your location and desination.
+Please provide your location and destination.
 
 We will warp a vessel toward your destination as soon as possible.
 
@@ -21,39 +22,44 @@ We will warp a vessel toward your destination as soon as possible.
 _Make sure to hold on to your wig, the wormholes opening, could suck it in_
 ''')
 
-
 pickup_date = st.date_input("Pickup Date", value=datetime.now().date())
 pickup_time = st.time_input("Pickup Time", value=datetime.now().time())
 pickup_datetime = datetime.combine(pickup_date, pickup_time).strftime("%Y-%m-%d %H:%M:%S")
 
-pickup_longitude = st.number_input("Pickup Longitude", value=-73.985428)
+# Default coordinates
+pickup_longitude = -73.985428
+pickup_latitude = 40.748817
+dropoff_longitude = -73.985428
+dropoff_latitude = 40.748817
 
-pickup_latitude = st.number_input("Pickup Latitude", value=40.748817)
+# Create a map centered around the default pickup location
+m = folium.Map(location=[pickup_latitude, pickup_longitude], zoom_start=15)
 
-dropoff_longitude = st.number_input("Dropoff Longitude", value=-73.985428)
+# Add markers for pickup and dropoff locations
+pickup_marker = folium.Marker([pickup_latitude, pickup_longitude], popup="Pickup Location", icon=folium.Icon(color="green"))
+dropoff_marker = folium.Marker([dropoff_latitude, dropoff_longitude], popup="Dropoff Location", icon=folium.Icon(color="red"))
 
-dropoff_latitude = st.number_input("Dropoff Latitude", value=40.748817)
+pickup_marker.add_to(m)
+dropoff_marker.add_to(m)
 
+# Display the map
+map_data = st_folium(m, width=700, height=500)
+
+# Update coordinates based on map click
+if map_data['last_clicked']:
+    if 'pickup' not in st.session_state:
+        st.session_state['pickup'] = True
+        pickup_longitude, pickup_latitude = map_data['last_clicked']['lng'], map_data['last_clicked']['lat']
+    else:
+        dropoff_longitude, dropoff_latitude = map_data['last_clicked']['lng'], map_data['last_clicked']['lat']
+        del st.session_state['pickup']
+
+# Display updated coordinates in input fields
+st.number_input("Pickup Longitude", value=pickup_longitude, key="pickup_longitude")
+st.number_input("Pickup Latitude", value=pickup_latitude, key="pickup_latitude")
+st.number_input("Dropoff Longitude", value=dropoff_longitude, key="dropoff_longitude")
+st.number_input("Dropoff Latitude", value=dropoff_latitude, key="dropoff_latitude")
 passenger_count = st.number_input("Number of Passengers", min_value=1, max_value=6, value=1)
-
-
-# def get_map_data():
-#     return pd.DataFrame(
-#         [[pickup_longitude, pickup_latitude], [dropoff_longitude, dropoff_latitude]],
-#         columns=['lon', 'lat']
-#     )
-
-# df = get_map_data()
-
-# st.map(df)
-
-pickup = pd.DataFrame({
-    'latitude': [pickup_latitude],
-    'longitude': [pickup_longitude]
-})
-
-st.map(pickup, zoom=15)
-
 
 url = 'https://taxifare.lewagon.ai/predict'
 
@@ -71,18 +77,3 @@ if st.button("Predict Fare"):
 
     prediction = response.json().get("fare", "Error")
     st.markdown(f"##  Fare: ${prediction:.2f}")
-
-# params = {
-#     'pickup_datetime': pickup_datetime,
-#     'pickup_longitude': pickup_longitude,
-#     'pickup_latitude': pickup_latitude,
-#     'dropoff_longitude': dropoff_longitude,
-#     'dropoff_latitude': dropoff_latitude,
-#     'passenger_count': passenger_count
-# }
-
-# response = requests.get(url, params=params)
-# prediction = response.json()
-
-# st.write(f"Prediction: {prediction}")
-# '''
---
+++ app_sample.py
@@ -0,0 +1,78 @@
+import streamlit as st
+import pandas as pd
+import requests
+import numpy as np
+from datetime import datetime
+
+
+'''
+# SPACE TRAVEL TAXI CO.
+'''
+
+st.markdown(f'''
+We are a space travel company,
+
+Please provide your location and desination.
+
+We will warp a vessel toward your destination as soon as possible.
+
+\n
+### WARNING
+_Make sure to hold on to your wig, the wormholes opening, could suck it in_
+''')
+
+
+pickup_date = st.date_input("Pickup Date", value=datetime.now().date())
+pickup_time = st.time_input("Pickup Time", value=datetime.now().time())
+pickup_datetime = datetime.combine(pickup_date, pickup_time).strftime("%Y-%m-%d %H:%M:%S")
+
+pickup_longitude = st.number_input("Pickup Longitude", value=-73.985428)
+
+pickup_latitude = st.number_input("Pickup Latitude", value=40.748817)
+
+dropoff_longitude = st.number_input("Dropoff Longitude", value=-73.985428)
+
+dropoff_latitude = st.number_input("Dropoff Latitude", value=40.748817)
+
+passenger_count = st.number_input("Number of Passengers", min_value=1, max_value=6, value=1)
+
+
+pickup = pd.DataFrame({
+    'latitude': [pickup_latitude],
+    'longitude': [pickup_longitude]
+})
+
+st.map(pickup, zoom=15)
+
+
+url = 'https://taxifare.lewagon.ai/predict'
+
+if st.button("Predict Fare"):
+    params = {
+        "pickup_datetime": pickup_datetime,
+        "pickup_longitude": pickup_longitude,
+        "pickup_latitude": pickup_latitude,
+        "dropoff_longitude": dropoff_longitude,
+        "dropoff_latitude": dropoff_latitude,
+        "passenger_count": passenger_count,
+    }
+
+    response = requests.get(url, params=params)
+
+    prediction = response.json().get("fare", "Error")
+    st.markdown(f"##  Fare: ${prediction:.2f}")
+
+# params = {
+#     'pickup_datetime': pickup_datetime,
+#     'pickup_longitude': pickup_longitude,
+#     'pickup_latitude': pickup_latitude,
+#     'dropoff_longitude': dropoff_longitude,
+#     'dropoff_latitude': dropoff_latitude,
+#     'passenger_count': passenger_count
+# }
+
+# response = requests.get(url, params=params)
+# prediction = response.json()
+
+# st.write(f"Prediction: {prediction}")
+# '''
