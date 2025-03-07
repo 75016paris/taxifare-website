eec29afaa07d376a51bafec2138461563a2ac5b7
Paul R <paulyacha@gmail.com>
2025-03-07 12:00:40 +0100

    no commit

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ Please provide your location and desination,
 The space shuttle will pick you up in asap
 ''')
 
-
 pickup_datetime = st.text_input('pickup datetime', '2014-07-06 19:18:00')
 
 pickup_longitude = st.text_input('pickup longitude')
@@ -37,16 +36,6 @@ passenger_count = st.number_input('passenger count', min_value=1, max_value=8, s
 
 # st.write(X_new)
 
-
-
-'''
-## Once we have these, let's call our API in order to retrieve a prediction
-
-See ? No need to load a `model.joblib` file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
-
-🤔 How could we call our API ? Off course... The `requests` package 💡
-'''
-
 url = 'https://taxifare.lewagon.ai/predict'
 
 params = {
@@ -62,15 +51,4 @@ response = requests.get(url, params=params)
 prediction = response.json()
 
 st.write(f"Prediction: {prediction}")
-'''
-
-########################################################################################################################
-
-2. Let's build a dictionary containing the parameters for our API...
-
-3. Let's call our API using the `requests` package...
-
-4. Let's retrieve the prediction from the **JSON** returned by the API...
-
-## Finally, we can display the prediction to the user
 '''
