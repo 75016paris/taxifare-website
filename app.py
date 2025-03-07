8e2fa884a5514d3feef5c0773d1df27617f242f2
Paul R <paulyacha@gmail.com>
2025-03-07 12:24:17 +0100

    no commit

--- app.py
+++ app.py
@@ -35,16 +35,7 @@ dropoff_latitude = st.number_input("Dropoff Latitude", value=40.748817)
 passenger_count = st.number_input("Number of Passengers", min_value=1, max_value=6, value=1)
 
 
-# X_new = pd.DataFrame(dict(date_and_time=[date_and_time],
-#                           pickup_longitude=[pickup_longitude],
-#                           pickup_latitude=[pickup_latitude],
-#                           dropoff_longitude=[dropoff_longitude],
-#                           dropoff_latitude=[dropoff_latitude],
-#                           passenger_count=[passenger_count]))
-
-# st.write(X_new)
-
-url = 'https://taxifare.lewagon.ai/predict'
+url = 'https://taxifare-154686505166.europe-west1.run.app'
 
 if st.button("Predict Fare"):
     params = {
